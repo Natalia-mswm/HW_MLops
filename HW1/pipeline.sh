9b6e40248a9fdaa295e1a60335a191980423357f
Natalia Maksimova <natalia-k.97@mail.ru>
2024-05-01 22:58:40 +0500

    update pipeline.ssh, changing path

--- HW1/pipeline.sh
+++ HW1/pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd ./ # Перейдем в текущую рабочую директорую, затем поочередно запустим со скриптами
+cd ./HW1/ # Перейдем в текущую рабочую директорую, затем поочередно запустим со скриптами
 python3 data_creation.py
 python3 model_preprocessing.py
 python3 model_preparation.py
